eb4d2fb56f7781ae88b50c42e4c27512259c4bc4
Blaise <menniablaise@yahoo.com>
2026-04-21 11:07:03 +0000

    fix: remove unnecessary backslash escaping in asset path strings across view templates

--- resources/views/pages/aboutpage/section5.blade.php
+++ resources/views/pages/aboutpage/section5.blade.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
     <link href="../assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset(\'assets/libs/css/style.css\') }}?v={{ filemtime(public_path(\'assets/libs/css/style.css\')) }}">
+    <link rel="stylesheet" href="{{ asset('assets/libs/css/style.css') }}?v={{ filemtime(public_path('assets/libs/css/style.css')) }}">
     <link rel="stylesheet" href="../assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
     <link rel="stylesheet" href="../assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
